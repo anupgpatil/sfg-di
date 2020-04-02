1fa87f9ef53430a136f3eefd98fd39014bd83df4
Anup <anupgpatil@gmail.com>
2020-04-01 22:52:11 -0700

    Initial Commit

---
+++ src/main/java/anup/learnspring/sfgdi/SfgDiApplication.java
@@ -0,0 +1,13 @@
+package anup.learnspring.sfgdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgDiApplication.class, args);
+	}
+
+}
