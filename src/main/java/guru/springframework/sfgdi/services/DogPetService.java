3148704fbea3d3581b6476d37a396f524da6dcdd
Anup <anupgpatil@gmail.com>
2020-04-14 22:11:19 -0700

    Assignment

--- src/main/java/guru/springframework/sfgdi/services/DogPetService.java
+++ src/main/java/guru/springframework/sfgdi/services/DogPetService.java
@@ -1,12 +1,16 @@
 package guru.springframework.sfgdi.services;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by jt on 12/28/19.
  */
 @Profile({"dog", "default"})
-public class DogPetService {
+@Qualifier("petService")
+@Service
+public class DogPetService implements PetService{
     public String getPetType(){
         return "Dogs are the best!";
     }
